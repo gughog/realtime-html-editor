a462e8e908ac9e4ce86b92d15b920a2ee72102c0
gughog <gustavogiovanne3@gmail.com>
2019-05-31 11:21:15 -0300

    fixed issues; highlight.js removed for now.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let codeArea = document.getElementById('code_area')
 
 codeArea.addEventListener('keyup', function(){
-    let codes = codeArea.innerText
+    let codes = codeArea.value
     let output = document.getElementById('code_output')
     output.srcdoc = codes
 })
