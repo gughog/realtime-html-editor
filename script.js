2e9b5aecf71815f12e0260d604033260d99f88c7
gughog <gustavogiovanne3@gmail.com>
2019-05-31 12:45:31 -0300

    added 'codemirror' for syntax highlight

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let codeArea = document.getElementById('code_area')
 
 codeArea.addEventListener('keyup', function(){
-    let codes = codeArea.value
+    let codes = codeArea.innerText
     let output = document.getElementById('code_output')
     output.srcdoc = codes
 })
+
+let codeEditor = CodeMirror(codeArea)
