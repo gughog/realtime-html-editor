28157c605024e53b4c08605327af906069e96e3a
gughog <gustavogiovanne3@gmail.com>
2019-06-06 02:39:21 -0300

    added all modifications for v2.0. VSCode Git has lost all my tracks of the development,  :(

---
+++ script.js
@@ -0,0 +1,26 @@
+let codeArea = document.getElementById('code_area')
+
+codeArea.addEventListener('keyup', function(){
+    let codes = codeArea.innerText
+    let output = document.getElementById('code_output')
+    output.srcdoc = codes
+})
+
+let codeEditor = CodeMirror(codeArea, {
+    /* theme: '3024-day', */
+    autofocus: true,
+    autoCloseBrackets: true,
+    autoCloseTags: true
+
+})
+
+let screenSize = screen.width
+console.log(screenSize)
+
+if(screenSize >= 768){
+    codeEditor.setSize(null, '84vh')
+} else {
+    codeEditor.setSize(null, '40vh')
+    let title = document.getElementById('title')
+    title.innerHTML = '< HTML Editor />'
+}
