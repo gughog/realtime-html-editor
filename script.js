d5b4d7a6f6b0594ff022addea63a520c8dea18e2
gughog <gustavogiovanne3@gmail.com>
2019-06-06 02:33:42 -0300

    Removed all old files

--- script.js
+++
@@ -1,9 +0,0 @@
-let codeArea = document.getElementById('code_area')
-
-codeArea.addEventListener('keyup', function(){
-    let codes = codeArea.innerText
-    let output = document.getElementById('code_output')
-    output.srcdoc = codes
-})
-
-let codeEditor = CodeMirror(codeArea)
